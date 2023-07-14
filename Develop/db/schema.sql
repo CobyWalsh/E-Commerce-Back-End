623d7e27e2c1b07970b669ffc73a8408c6999ee3
coby walsh <cobywalsh7@yahoo.com>
2023-07-14 15:45:21 -0600

    initial commit

---
+++ Develop/db/schema.sql
@@ -0,0 +1,11 @@
+-- DROP DATABASE
+DROP DATABASE IF EXISTS ecommerce_db;
+
+-- CREATE DATABASE
+CREATE DATABASE ecommerce_db;
+USE ecommerce_db;
+
+CREATE TABLE category (
+    id INT NOT NULL
+    PRI
+)
