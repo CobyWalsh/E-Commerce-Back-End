762bc9090c3b4a8a39114250428a50962cc7bd39
coby walsh <cobywalsh7@yahoo.com>
2023-07-15 09:36:32 -0600

    added code to routes/api and I am currently trying to get npm run seed to seed the database

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -3,13 +3,5 @@ DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
-USE ecommerce_db;
-
-CREATE TABLE category (
-    id INT NOT NULL,
-    PRIMARY KEY (id),
-    AUTO_INCREMENT,
-    category_name varchar(100)
-);
 
 
